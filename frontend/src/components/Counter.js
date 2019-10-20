26e84598681db406670bfe6093efb5dc7e401a69
Calvin Fronda <cfronda209@gmail.com>
2019-10-20 15:57:54 -0700

    Inital timer

--- frontend/src/components/Counter.js
+++ frontend/src/components/Counter.js
@@ -1,41 +1,73 @@
 import React, { Component } from 'react';
+import Timer from './Timer'
+import Option from './Option'
 
 class Counter extends Component {
   constructor() {
     super();
     this.state = {
-      timerTime: 1,
       timerOn: false,
-      view: []
+      breakTime: 250,
+      workTime: 1500,
+      seconds: 1500
     };
+    this.playStop = this.playStop.bind(this);
+    this.updateTime = this.updateTime.bind(this);
   }
 
-  componentDidMount() {
-    this.millisToMinutesAndSeconds();
-  }
+  updateTime() {
+    this.setState(function (prevState, props) {
+      const currentState = Object.assign(prevState);
+      const stillActive = (prevState.seconds - 1) > 0;
+      const nextTimer = prevState.active === 'workTime' ? 'breakTime' : 'workTime'
 
-  millisToMinutesAndSeconds() {
-    let time = 1500000;
-    var minutes = Math.floor(time / 60000);
-    var seconds = ((time % 60000) / 1000).toFixed(0);
-    let countddown = minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-    this.setState({
-      view: countddown
+      currentState.seconds = stillActive ? currentState.seconds - 1 : currentState[nextTimer];
+      currentState.active = stillActive ? currentState.active : nextTimer;
+      if (this.timerID) {
+        currentState.timerId = this.timerID;
+      }
+      return currentState;
     });
   }
+  playStop() {
+    if (this.state.timerId) {
+      clearInterval(this.state.timerId);
+      return this.setState({
+        seconds: this.state.workTime,
+        timerId: false,
+        active: 'workTime'
+      });
+    }
+
+    this.timerID = setInterval(() => this.updateTime(), 1000)
+  }
+  // 
+  updateLength(timer, e) {
+    if (this.state.timerId) {
+      return false;
+    }
+
+    const state = Object.assign({}, this.state);
+    state[timer] = e.target.value * 60;
+    state.seconds = timer === 'workTime' ? e.target.value * 60 : state.seconds
+    this.setState(state);
+  }
+
 
   render() {
+    const buttonString = this.state.timerId ? 'Stop' : 'Start';
+    const Button = (props) => <button className="btn" onClick={props.action}>{props.children}</button>
     return (
-      <div className='mainCounterContainer'>
-        <h1 className='counter'>{this.state.view}</h1>;
-        <div className='buttonActions'>
-          <button onClick={this.startTimer}>Start</button>
+      <div className="app">
 
-          <button>Break</button>
-        </div>
+        <Timer active={this.state.active} seconds={this.state.seconds} />
+        <Button action={this.playStop}>{buttonString}</Button>
+        <Option value={this.state.workTime} timer="workTime" updateLength={this.updateLength.bind(this)}>Minutes of work</Option>
+        <Option value={this.state.breakTime} timer="breakTime" updateLength={this.updateLength.bind(this)}>Minutes of break</Option>
       </div>
-    );
+    )
   }
 }
+
 
 export default Counter;
